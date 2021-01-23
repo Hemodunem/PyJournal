fd72d0000d087a2260813fc26144cab1408aa59c
Sasha Kulikov <Hemodunem@yandex.ru>
2021-01-23 13:49:20 +0300

    фвц

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from main.models import Student, Teacher, Subject, Mark
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Mark)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60, default = 'name')
+    surname = models.CharField(max_length=60, default = 'surname')
+    patronymic = models.CharField(max_length=60, default = 'patronymic')
+
+    def __str__(self):
+        return self.surname + ' ' + self.name + ' ' + self.patronymic
+
+
+class Student(Person):
+    year = models.IntegerField(default=1)
+    pass
+
+
+class Teacher(Person):
+    pass
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Mark(models.Model):
+    value = models.IntegerField()
+    data = models.DateField()
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING, null = True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING, null = True)
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING, null = True)
