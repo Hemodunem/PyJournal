81c5853bdcc04730f5dfc28a22b3a2587f79bd8a
Sasha Kulikov <Hemodunem@yandex.ru>
2021-01-30 14:03:50 +0300

    change

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login/', views.loginPage),
-    path('register/', views.registerPage),
+    path('', views.index, name='home'),
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from main.forms import CreateUserForm
@@ -9,20 +10,45 @@ from main.forms import CreateUserForm
 def index(request):
 
     context = {
-        'values': ['Test 1', 'Test 2']
+        'values': ['Test 1', 'Test 2'],
+        'username': request.user.username
     }
 
     return render(request, 'main/index.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, 'Username OR password is incorrect')
+
     return render(request, 'main/login.html')
 
-
 def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            print(form.is_valid)
+            form.save()
+            return redirect('login')
+
 
     context = {
-        'form':form
+        'form': form
     }
-    return render(request, 'main/register.html')
+    return render(request, 'main/register.html', context)
